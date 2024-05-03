ea62536d4e34bdf40e6560ac4d6f17df926a2a96
Sruthika Sivakumar <110161415+Sruttz@users.noreply.github.com>
2024-05-03 13:04:37 +0530

    Update CNutrition Recommendation.py

--- pages/CNutrition Recommendation.py
+++ pages/CNutrition Recommendation.py
@@ -17,22 +17,30 @@ def get_gemini_response(question):
     response = chat.send_message(question, stream=True)
     return response
 
-
 # Initialize Streamlit app
 st.set_page_config(page_title="Nutrition Recommendation")
 st.title("Nutrition Plan")
 
-# Input for meal type
+# Input for fitness goal
 goal_type = st.selectbox("Select your fitness goal:", ["Weight loss", "Muscle gain", "Endurance improvement", "Overall health and wellness"])
 
-# Button to generate recipe
+# Input for age
+age = st.number_input("Enter your age:", min_value=10, max_value=100, step=1)
+
+# Button to generate nutrition plan
 submit_button = st.button("Generate Nutrition Plan")
 
-# Check if input_text is not empty and button is clicked
-if goal_type and submit_button:
+# Check if input is not empty and button is clicked
+if goal_type and age and submit_button:
     st.subheader("Here's the Nutrition Plan:")
     # Create prompt based on user inputs
-    prompt = f"Generate a nutrition plan for {goal_type.lower()} "
+    prompt = f"Generate a nutrition plan for {goal_type.lower()} individuals aged {age} "
     response = get_gemini_response(prompt)
-    for chunk in response:
-        st.write(chunk.text)
+    if response:
+        for chunk in response:
+            if hasattr(chunk, "text"):
+                st.write(chunk.text)
+            else:
+                st.warning("Unable to generate nutrition plan. Please try again later.")
+    else:
+        st.warning("Unable to generate nutrition plan. Please try again later.")
